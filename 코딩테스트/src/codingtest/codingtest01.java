5834820cb95d6026f9d3151a7cd495a5c912ffa4
Kim <Kim@222.102.11.159>
2022-12-25 17:49:12 +0900

    코딩테스트0레벨 박테리아증식문제

---
+++ 코딩테스트/src/codingtest/codingtest01.java
@@ -0,0 +1,27 @@
+package codingtest;
+
+import java.util.ArrayList;
+
+public class codingtest01 {
+
+	public static void main(String[] args) {
+	
+		
+	int result = bacteria(2,10);
+	
+		System.out.println(result);	 	 	 		
+		
+
+	}
+	
+	public static int bacteria(int num, int time) {	
+		
+			
+		for(int i =1; i<=time;i++) {			
+			num=num*2;						
+		}		
+				
+		return num;
+	}
+
+}
