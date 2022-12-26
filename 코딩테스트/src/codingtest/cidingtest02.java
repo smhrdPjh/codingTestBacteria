f9b91a201245f58b7f63eaf85e3093a3ee9249f9
Kim <Kim@222.102.11.159>
2022-12-26 22:06:22 +0900

    편지문제

---
+++ 코딩테스트/src/codingtest/cidingtest02.java
@@ -0,0 +1,14 @@
+package codingtest;
+
+public class cidingtest02 {
+
+	public static void main(String[] args) {
+
+
+		String message = "happy birthday!";
+		int messageSize = message.length();
+		System.out.println(messageSize);
+
+	}
+
+}
