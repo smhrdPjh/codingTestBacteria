434288f13cff7e0b370f61074968bff3a360aab4
Kim <Kim@121.178.89.52>
2022-12-29 01:18:05 +0900

    피나보치 수

---
+++ 코딩테스트/src/codingtest/피나보치.java
@@ -0,0 +1,30 @@
+package codingtest;
+
+public class 피나보치 {
+
+	public static void main(String[] args) {
+		int check = pinabocci(3);
+		System.out.println("결과:" + check);
+
+	}
+
+	public static int pinabocci(int n) {
+
+		int result = 0;
+		int[] arr = new int[n + 1];
+
+		arr[0] = 0;
+		arr[1] = 1;
+
+		for (int i = 2; i <= n; i++) {
+
+			arr[i] = (arr[i - 1]%1234567) + (arr[i - 2]%1234567);
+
+			
+		}
+		
+
+		return arr[n]%1234567;
+	}
+
+}
